dfe443eecb817d5de86e49e096a53712852cfa9e
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-06-18 19:20:28 +0530

    Update program3.cpp

--- program3.cpp
+++ program3.cpp
@@ -1,1 +1,13 @@
-
+Develop a program to demonstrate basic geometric Operationson 3D Objectin C++ using GLUT
+ #include <GL/glut.h>
+ #include <iostream>
+ GLfloat cubeSize = 1.0f;
+ GLfloat cubePosX = 0.0f, cubePosY = 0.0f, cubePosZ = -5.0f;
+ GLfloat anglex = 0.0f, angley = 0.0f;
+ GLfloat scale = 1.0f;
+  void drawCube() { 
+  glPushMatrix();
+  glTranslatef (cubePosX, cubePosy, cubePosZ);
+  glRotatef (anglex, 1.0f, 0.0f, 0.0f);
+  lRotatef (angley, 0.0f, 1.0f, 0.0f);
+  glScalef(scale, scale, scale);
