fa81de4caa99258a922035361e9f7c88b7f0a5b1
Pallavi <126679884+Pallavi-Shtgr@users.noreply.github.com>
2024-06-19 07:03:35 +0530

    Update program3.cpp

--- program3.cpp
+++ program3.cpp
@@ -118,3 +118,31 @@ break;
  
 default:
 break;
+
+glutPostRedisplay();
+
+123
+
+124 int main(int argc, char **argv) {
+
+125 glutInit(&argc, argv);
+
+126 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB II
+
+GLUT_DEPTH); glutInitWindowSize(500, 500); 127
+
+A128 glutCreateWindow("Geometric Operations on 3D Object");
+
+129 init();
+
+130 glutDisplayFunc(display);
+
+glutKeyboardFunc(keyboard);
+
+?
+
+132 glutMainLoop();
+
+133 return 0;
+
+134}
